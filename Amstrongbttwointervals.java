321bc3cd116c0088c09bfaf4c507757ad00ddd0d
pavithra-6 <paviikannan1996@gmail.com>
2017-07-24 04:54:28 -0700

    Add files via upload

---
+++ Amstrongbttwointervals.java
@@ -0,0 +1,41 @@
+package guvi;
+import java.util.Scanner;
+public class Amstrongbttwointervals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n, count = 0, a, b, c, sum = 0;
+
+        System.out.print("Armstrong numbers from 1 to 1000:");
+
+        for(int i = 1; i <= 1000; i++)
+
+        {
+
+            n = i;
+
+            while(n > 0)
+
+            {
+
+                b = n % 10;
+
+                sum = sum + (b * b * b);
+
+                n = n / 10;
+
+            }
+
+            if(sum == i)
+
+            {
+
+                System.out.println(i+" ");
+
+            }
+
+            sum = 0;
+
+        }
+}
+	}
